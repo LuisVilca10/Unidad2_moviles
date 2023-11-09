56f1ccf9501415d98b138aedd7824fcc25aa6367
luisvilca10 <martinvilca@upeu.edu.pe>
2023-11-09 05:27:13 -0500

    examenu2

--- asistenciaupeu_frontend/android/app/src/main/kotlin/com/example/asistenciaupeu_frontend/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asistenciaupeu_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- asistenciaupeu_frontend/lib/util/UrlApi.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: file_names
-class UrlApi {
-  UrlApi._();
-  static const String urlApix = "http://192.168.0.103:8083";
-}
